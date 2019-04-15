f8066e6048e89a48e5616d4d95be431faca75eba
Kristen Newbury <knewbury@ualberta.ca>
2019-04-14 21:15:34 -0600

    re-add omissions to extract classes sets to analyze, somehow tests and libs still in there

--- extractClasses.sh
+++ extractClasses.sh
@@ -5,7 +5,7 @@
 ##################################
 rm allclasses.txt
 
-find . -path '*/classes' >> allclasses.txt
+find . -path '*/classes' | grep -v "\<test\>" | grep -v "\<lib\>" | grep -v "\<dep\>" >> allclasses.txt
 total=$(cat allclasses.txt | wc -l)
 
 echo "The number of collected analysis inputs is: $total"
